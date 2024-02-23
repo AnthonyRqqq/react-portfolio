2a0f697161310dbee9228b90b72c7bb68f387e8f
unknown <anthony_rosati@yahoo.com>
2024-02-22 23:24:43 -0600

    Added data to projects.js

---
+++ src/components/projects.js
@@ -0,0 +1,27 @@
+
+// List of project data to be displayed
+const projects = [
+    {
+        name: 'TasteBuds',
+        githubURL: 'https://github.com/Aaron-Heath/tastebuds',
+        deployedURL: 'https://tastebuds-app-de96bd264a71.herokuapp.com/',
+    },
+    {
+        name: 'Box Office Dojo',
+        githubURL: 'https://github.com/Mb739132/Box-Office-Dojo',
+        deployedURL: 'https://mb739132.github.io/Box-Office-Dojo/',
+    },
+    {
+        name: 'Weather Dashboard',
+        githubURL: 'https://github.com/AnthonyRqqq/weather-dashboard',
+        deployedURL: 'https://anthonyrqqq.github.io/weather-dashboard/',
+    },
+    {
+        name: 'SVG Logo Maker',
+        githubURL: 'https://github.com/AnthonyRqqq/svg-logo-make',
+        // No front-end visibility, same link to github for convenience in react formatting
+        deployedURL: 'https://github.com/AnthonyRqqq/svg-logo-make',
+    },
+]
+
+export default projects;
