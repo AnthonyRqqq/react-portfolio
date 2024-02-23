2f129c94235064031b594d9b485eca525ffe0f55
unknown <anthony_rosati@yahoo.com>
2024-02-22 23:48:46 -0600

    Added code to Navigation.jsx

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,39 @@
+
+import { Link, useLocation } from 'react-router-dom';
+
+function Navigation() {
+    const currentPage = useLocation().pathName;
+
+    return (
+        <ul className='nav'>
+            <li className='nav-item'>
+                <Link
+                    // Navigation link to another url
+                    to='/'
+                    // Checks current page url and if matching sets the nav-link to active status
+                    className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+                >About Me</Link>
+            </li>
+            <li className='nav-item'>
+                <Link
+                    to='/Projects'
+                    className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+                >Projects</Link>
+            </li>
+            <li className='nav-item'>
+                <Link
+                    to='/Contact'
+                    className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+                >Contact</Link>
+            </li>
+            <li className='nav-item'>
+                <Link
+                    to='/Resume'
+                    className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+                >Resume</Link>
+            </li>
+        </ul>
+    );
+}
+
+export default Navigation;
