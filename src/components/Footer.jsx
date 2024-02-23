4305f91a9ae1e76fa31d548fe4581aae93369463
unknown <anthony_rosati@yahoo.com>
2024-02-22 23:54:29 -0600

    Added placeholder code to Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <ul>
+            <li>Placeholder Img 1</li>
+            <li>Placeholder Img 2</li>
+            <li>Placeholder Img 3</li>
+        </ul>
+    );
+};
