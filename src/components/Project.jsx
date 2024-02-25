c151d61df344b52cdda382137d111e31eccddf40
unknown <anthony_rosati@yahoo.com>
2024-02-24 21:31:24 -0600

    Simplified project.jsx code

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,17 +1,11 @@
-import React from 'react';
-import { projects } from '../projects';
-
-export default function Project({ projects }) {
+export default function Project({ project }) {
     return (
         <div className='container'>
             <h3>Projects</h3>
             <ul className='list-group'>
-                {/* Use map method to iterate through each project and return array of list items*/}
-                {projects.map((project) => (
                     <li className='list-group-item' key={project.name}>
                         {`${project.name}, ${project.githubURL}, ${project.deployedURL}`}
                     </li>
-                ))}
             </ul>
         </div>
     );
