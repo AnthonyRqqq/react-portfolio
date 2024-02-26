cb79b6b89365a402dc6a8274d00ca53710ac83c1
unknown <anthony_rosati@yahoo.com>
2024-02-25 21:12:06 -0600

    Styling for footer logos added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,19 +6,19 @@ import linkedinLogo from '../assets/images/footerImages/linkedin.jpg'
 
 export default function Footer() {
     return (
-        <ul>
+        <ul className='logo-list row'>
             {/* Render individual logos with appropriate links */}
-            <li>
+            <li className='col-4'>
                 <a href='https://github.com/AnthonyRqqq'>
                     <img className='img-responsive' src={githubLogo} alt='github image with link' />
                 </a>
             </li>
-            <li>
+            <li className='col-4'>
                 <a href='https://www.linkedin.com/in/anthony-rosati-30b616149/'>
                     <img className='img-responsive' src={linkedinLogo} alt='linkedin image with link' />
                 </a>
             </li>
-            <li>
+            <li className='col-4'>
                 <a href='https://www.codewars.com/users/AnthonyRqqq'>
                     <img className='img-responsive' src={codewarsLogo} alt='codewars image with link' />
                 </a>
