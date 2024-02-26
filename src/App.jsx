4bcb6e7acb94ed75a42927d26b3ca9ebc004f76a
unknown <anthony_rosati@yahoo.com>
2024-02-25 21:04:41 -0600

    Added functionality to footer logos

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import { Outlet } from 'react-router-dom';
 import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 import './App.css'
 
 export default function App() {
   return (
     <>
       <header>
+        <h3>
         <Navigation />
+        </h3>
       </header>
-      <main className='mx-3'>
+
+      <main>
         <Outlet />
       </main>
+
+      {/* Locks footer at bottom of page and centers */}
+      <footer className='footer d-flex justify-content-center'>
+        <Footer />
+      </footer>
     </>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,28 @@
+// Import logo images
+import githubLogo from '../assets/images/footerImages/github.jpg'
+import codewarsLogo from '../assets/images/footerImages/codewars.jpg'
+import linkedinLogo from '../assets/images/footerImages/linkedin.jpg'
+
+
 export default function Footer() {
     return (
         <ul>
-            <li>Placeholder Img 1</li>
-            <li>Placeholder Img 2</li>
-            <li>Placeholder Img 3</li>
+            {/* Render individual logos with appropriate links */}
+            <li>
+                <a href='https://github.com/AnthonyRqqq'>
+                    <img className='img-responsive' src={githubLogo} alt='github image with link' />
+                </a>
+            </li>
+            <li>
+                <a href='https://www.linkedin.com/in/anthony-rosati-30b616149/'>
+                    <img className='img-responsive' src={linkedinLogo} alt='linkedin image with link' />
+                </a>
+            </li>
+            <li>
+                <a href='https://www.codewars.com/users/AnthonyRqqq'>
+                    <img className='img-responsive' src={codewarsLogo} alt='codewars image with link' />
+                </a>
+            </li>
         </ul>
     );
 };
