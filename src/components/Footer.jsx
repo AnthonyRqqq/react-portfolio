ff6f51a1235502953234146c65b409e83d2103ff
unknown <anthony_rosati@yahoo.com>
2024-02-25 21:13:26 -0600

    Updated footer logo links to open new tabs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,19 +7,19 @@ import linkedinLogo from '../assets/images/footerImages/linkedin.jpg'
 export default function Footer() {
     return (
         <ul className='logo-list row'>
-            {/* Render individual logos with appropriate links */}
+            {/* Render individual logos with appropriate links, opens new tab when clicked */}
             <li className='col-4'>
-                <a href='https://github.com/AnthonyRqqq'>
+                <a target='_blank' href='https://github.com/AnthonyRqqq'>
                     <img className='img-responsive' src={githubLogo} alt='github image with link' />
                 </a>
             </li>
             <li className='col-4'>
-                <a href='https://www.linkedin.com/in/anthony-rosati-30b616149/'>
+                <a target='_blank' href='https://www.linkedin.com/in/anthony-rosati-30b616149/'>
                     <img className='img-responsive' src={linkedinLogo} alt='linkedin image with link' />
                 </a>
             </li>
             <li className='col-4'>
-                <a href='https://www.codewars.com/users/AnthonyRqqq'>
+                <a target='_blank' href='https://www.codewars.com/users/AnthonyRqqq'>
                     <img className='img-responsive' src={codewarsLogo} alt='codewars image with link' />
                 </a>
             </li>
