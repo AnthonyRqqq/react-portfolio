b1aa3031e798d4ebcaedeb45ef30cb12f88fc146
unknown <anthony_rosati@yahoo.com>
2024-02-26 17:14:29 -0600

    Functionality added to Resume.jsx, added pdf resume for downloads to assets folder

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,7 +1,41 @@
+import resumeDownload from '../assets/Anthony-Rosati-Resume.pdf'
+
 export default function Resume() {
     return (
         <div>
             <h3>Resume Page</h3>
+            {/* Downloadable resume */}
+            <p>Download my resume
+                <a href={resumeDownload} download> here.</a>
+            </p>
+
+            {/* Front end skills list */}
+            <div>
+                <h4>Front End Proficiencies</h4>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>JQuery</li>
+                    <li>Responsive Design</li>
+                    <li>React</li>
+                    <li>Bootstrap</li>
+                </ul>
+            </div>
+
+            <div>
+                <h4>Back End Proficiences</h4>
+                <ul>
+                    <li>APIs</li>
+                    <li>Node</li>
+                    <li>Express</li>
+                    <li>MySQL, Sequelize</li>
+                    <li>MongoDB, Mongoose</li>
+                    <li>REST</li>
+                    <li>GraphQL</li>
+                </ul>
+            </div>
         </div>
+
     )
 }
