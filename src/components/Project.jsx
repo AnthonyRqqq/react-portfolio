ce7e2d254712b998cf9dac19b738e3c29c2d5ee1
unknown <anthony_rosati@yahoo.com>
2024-02-25 22:13:10 -0600

    Refactored Project.jsx code to have links to both deployed project and github repos, initial additions to CSS for Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,18 +1,25 @@
+import githubLogo from '../assets/images/footerImages/github.jpg'
 
 export default function Project({ projects }) {
     return (
-        <div className='container'>
-            <h3>Projects</h3>
-            <ul className='list-group row'>
+        <div className='container project-list-container'>
+            <h3 className='justify-content-center d-flex'>Projects</h3>
+            <ul className='project-list row'>
+
                 {/* Use map method to iterate through each project and return array of list items*/}
                 {projects.map((project) => (
-                    <li className='list-group-item col d-flex project-li' key={project.name}>
-                        {`${project.name}`}
-                        <a target='_blank' href={project.githubURL}>
-                            <img className='img-responsive' src={project.screenshot} alt={project.imageText} />
+                    <li className='col-6 project-li' key={project.name}>
+                        {/* Creates link to deployed project */}
+                        <a className='deployed-link' target='_blank' href={project.deployedURL}>{project.name}</a>
+
+                        {/* Creates link to github repo of project */}
+                        <a className='project-github-link' target='_blank' href={project.githubURL}>
+                            <img className='img-responsive project-github-link-image' src={githubLogo} alt='link to github repository' />
                         </a>
+                        <img className='img-responsive project-screenshot' src={project.screenshot} alt={project.imageText} />
                     </li>
-                ))}
+
+                ))};
             </ul>
         </div>
     );
